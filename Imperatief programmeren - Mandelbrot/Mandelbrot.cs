588c59b5f6675ffed4325aa81fdad307af345e52
omgjens <jlangen>
2013-09-25 13:14:19 +0200

    Add form
    Clean code

--- Imperatief programmeren - Mandelbrot/Mandelbrot.cs
+++ Imperatief programmeren - Mandelbrot/Mandelbrot.cs
@@ -1,73 +1,78 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
-class Mandelbrot : Form
+namespace Mandelbrot
 {
-    public Mandelbrot()
+    public partial class Mandelbrot : Form
     {
-        const int width = 300;
-        const int height = 300;
-        int n;
-        double scale;
+        // Declaration of the globals
         Bitmap mandelbrot;
-        PictureBox pictureBox;
 
-        mandelbrot = new Bitmap(width, height);
-        pictureBox = new PictureBox();
-        pictureBox.Size = new Size(width, height);
-        pictureBox.Image = mandelbrot;
-        scale = 0.004;
-
-        // For every x and y run the mandelbrot calculation
-        for (int x = 0; x < width; x++)
+        // Constructor
+        public Mandelbrot() 
         {
-            for (int y = 0; y < height; y++)
-            {
-                n = CalculateMandelNumber(x * scale, y * scale);
-                mandelbrot.SetPixel(x, y, (n % 2 == 0) ? Color.White : Color.Black);
-            }
+            InitializeComponent();
+            mandelbrot = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            DrawMandelbrot();
+
         }
 
-        this.Controls.Add(pictureBox);
-    }
-
-    private static int CalculateMandelNumber(double x, double y)
-    {
-        double a, b, ra, rb;
-        int result;
-        a = 0;
-        b = 0;
-        result = 0;
-
-        while (DistanceToOrigin(a, b) < 2.0)
+        // Calculate and draw the mandelbrot figure
+        private void DrawMandelbrot()
         {
-            if (result == 100)
+            int n;
+            double scale;
+            scale = 0.004;
+
+            // For every x and y run the mandelbrot calculation
+            for (int x = 0; x < pictureBox1.Width; x++)
             {
-                break;
+                for (int y = 0; y < pictureBox1.Height; y++)
+                {
+                    n = CalculateMandelNumber(x * scale, y * scale);
+                    mandelbrot.SetPixel(x, y, (n % 2 == 0) ? 
+                        Color.White : Color.Black);
+                }
             }
 
-            // The Mandelbrot algorithm
-            ra = a * a - b * b + x;
-            rb = 2 * a * b + y;
-
-            a = ra;
-            b = rb;
-            result++;
+            pictureBox1.Image = mandelbrot;
         }
 
-        return result;
-    }
+        // Method that calculates the Mandelnumber for a given x and y using 
+        // the Mandelbrot algorithm
+        private static int CalculateMandelNumber(double x, double y)
+        {
+            double a, b, ra, rb;
+            int result;
+            a = 0;
+            b = 0;
+            result = 0;
 
-    private static double DistanceToOrigin(double x, double y)
-    {
-        // Calculate the shortest distance from point (x, y) to point (0, 0) using Pythagoras
-        return Math.Sqrt(x * x + y * y);
-    }
+            while (DistanceToOrigin(a, b) < 2.0)
+            {
+                if (result == 100)
+                {
+                    break;
+                }
 
-    public static void Main()
-    {
-        Application.Run(new Mandelbrot());
+                // The Mandelbrot algorithm
+                ra = a * a - b * b + x;
+                rb = 2 * a * b + y;
+
+                a = ra;
+                b = rb;
+                result++;
+            }
+
+            return result;
+        }
+
+        // Calculate the shortest distance from point (x, y) to point (0, 0)
+        // using Pythagoras
+        private static double DistanceToOrigin(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
     }
 }
